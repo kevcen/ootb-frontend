c69c100210fc728ca2a1710343d18b36b5ae8253
Jack Q <jljqiuc@gmail.com>
2021-06-03 20:46:29 +0100

    Add navigation between intro and interest screen

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -9,19 +11,29 @@ import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import QuestionScreen from './src/screens/QuestionScreen' ;
 import IntroScreen from './src/screens/IntroScreen';
+import InterestScreen from './src/screens/InterestScreen';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  const Stack = createStackNavigator();
+
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <IntroScreen />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={IntroScreen}
+            />
+            <Stack.Screen
+              name="Interest"
+              component={InterestScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
--- src/screens/IntroScreen.tsx
+++ src/screens/IntroScreen.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { StyleSheet, View, Text, Button} from 'react-native';
-import Question from '../components/Question'
+import Question from '../components/Question' ;
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 // import { Container, Header, Content, Button } from 'native-base';
 
 
-export default function IntroScreen() {
+export default function IntroScreen({navigation} : {navigation : any}) {
   return (
     <View style={styles.question}>
       <Question questionText={"Please tell us a bit about more about the receiver"}/>
-      <Button onPress={() => {}} title="Let's go" />
+      <Button onPress={() => {navigation.navigate('Interest')}} title="Let's go" />
     </View>
   );
 }
