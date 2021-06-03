3c0e393b71fc75cd2a1b144a789fbee69454b4c8
Jack Q <jljqiuc@gmail.com>
2021-06-03 21:33:59 +0100

    Add disappearing buttons upon click in InterestScreen

--- src/screens/InterestScreen.tsx
+++ src/screens/InterestScreen.tsx
@@ -3,21 +3,31 @@ import { StyleSheet, View, Text, Button} from 'react-native';
 import Question from '../components/Question' ;
 import {interests} from "../constants/Interests" ;
 
-export default function InterestScreen() {
+export default function InterestScreen({navigation} : {navigation : any}) {
   
   var interestButtons : any[] = [];
 
+  var gifteeInterests = [];
+
   interests.forEach(element => {
+    const [showButton, setShowButton] = React.useState(true);
+
     interestButtons.push(
-      <Button onPress={() => {}} title={element} />
-    )
+      (showButton ? (<Button onPress={() => {
+        gifteeInterests.push({element})
+        setShowButton(!showButton)
+      }} title={element} />
+    ) : null))
   });
   
   return (
     <View style={styles.question}>
       <Question questionText={"What's their interest"}/>
       {interestButtons}
-      <Button onPress={() => {}} title="Let's go" />
+      <Button onPress={() => {
+        // send post request to backend server
+        navigation.navigate('Interest')
+      }} title="Let's go" />
     </View>
   );
 }
