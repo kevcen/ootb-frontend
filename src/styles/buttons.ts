783e11e84ddc3135b0b202e43a11247f0df28b3a
Jack Q <jljqiuc@gmail.com>
2021-06-03 23:09:45 +0100

    [kkc19][jlq19] Add styles folder for easier style structuring

---
+++ src/styles/buttons.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const buttonStyles = StyleSheet.create({
+  blackCenteredFull: {
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  blackCenteredDiminish: {
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
