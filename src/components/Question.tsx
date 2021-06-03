cf84c7b0d4e64424129bb92d55948327fae3e3b4
Jack Q <jljqiuc@gmail.com>
2021-06-03 19:34:08 +0100

    Add question component and screen

---
+++ src/components/Question.tsx
@@ -0,0 +1,24 @@
+
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import Colors from '../constants/Colors';
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
+
+
+export default function Question({questionText} : {questionText: string}) {
+  return (
+    <View>
+      <Text style={styles.titleText}>{questionText}
+      </Text> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 20
+  }
+})
