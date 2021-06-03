a873595a5d6acad96bd658917d42451216d89e6c
Jack Q <jljqiuc@gmail.com>
2021-06-03 23:26:45 +0100

    Remove unused imports from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
 import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
 
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
