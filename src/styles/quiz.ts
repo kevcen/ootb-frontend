f2a825257baca614cb927b51cc12d4af9779b280
Jack Q <jljqiuc@gmail.com>
2021-06-09 13:14:27 +0100

    Remove duplicate style in styles/quiz.ts

--- src/styles/quiz.ts
+++ src/styles/quiz.ts
@@ -1,87 +1,44 @@
 import { StyleSheet, View, Text, Button, Pressable } from "react-native";
 
 export const styles = StyleSheet.create({
-    viewCentered: {
-      flex: 1,
-      alignItems: "center",
-      backgroundColor: "white",
-    },
-    space: {
-      width: 20,
-      height: 20,
-    },
-    list: {
-      flexDirection: "row",
-      flexWrap: "wrap",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    tag: {
-      height: 40,
-      marginVertical: 5,
-      marginHorizontal: 3,
-      paddingHorizontal: 20,
-      borderRadius: 50,
-      borderWidth: 2,
-      borderColor: "white",
-      alignItems: "center",
-      justifyContent: "center",
-      opacity: 0.4,
-    },
-    tagChosen: {
-      height: 40,
-      marginVertical: 5,
-      marginHorizontal: 3,
-      paddingHorizontal: 20,
-      borderRadius: 50,
-      borderWidth: 2,
-      borderColor: "black",
-      alignItems: "center",
-      justifyContent: "center",
-      opacity: 0.8,
-    },
-  });
-
-
-  // const styles = StyleSheet.create({
-  //   viewCentered: {
-  //     flex: 1,
-  //     alignItems: "center",
-  //     backgroundColor: "white",
-  //   },
-  //   space: {
-  //     width: 20,
-  //     height: 20,
-  //   },
-  //   list: {
-  //     flexDirection: "row",
-  //     flexWrap: "wrap",
-  //     justifyContent: "center",
-  //     alignItems: "center",
-  //   },
-  //   tag: {
-  //     height: 40,
-  //     marginVertical: 5,
-  //     marginHorizontal: 3,
-  //     paddingHorizontal: 20,
-  //     borderRadius: 50,
-  //     borderWidth: 2,
-  //     borderColor: "white",
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //     opacity: 0.4,
-  //   },
-  //   tagChosen: {
-  //     height: 40,
-  //     marginVertical: 5,
-  //     marginHorizontal: 3,
-  //     paddingHorizontal: 20,
-  //     borderRadius: 50,
-  //     borderWidth: 2,
-  //     borderColor: "black",
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //     opacity: 0.8,
-  //   },
-  // });
-  
+  viewCentered: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+  list: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  tag: {
+    height: 40,
+    marginVertical: 5,
+    marginHorizontal: 3,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.4,
+  },
+  tagChosen: {
+    height: 40,
+    marginVertical: 5,
+    marginHorizontal: 3,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.8,
+  },
+});
