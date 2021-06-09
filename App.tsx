08479fe6ffc5b482701f45e3ec1521f7863d9df5
Jack Q <jljqiuc@gmail.com>
2021-06-09 22:47:32 +0100

    Change route navigation to include BudgetScreen

--- App.tsx
+++ App.tsx
@@ -65,6 +65,7 @@ export default function App() {
             name="Recommendations"
             component={RecommendationScreen}
           />
+          <Stack.Screen name="Budget" component={BudgetScreen} />
           <Stack.Screen name="Error" component={ErrorScreen} />
         </Stack.Navigator>
       </NavigationContainer>
