f2cdb24eea3e0ed4e530f58e232960059fd74def
kev <kev9821@hotmail.co.uk>
2021-06-10 15:32:57 +0100

    switch back to remote post than localhost

--- src/screens/RecommendationScreen.tsx
+++ src/screens/RecommendationScreen.tsx
@@ -34,7 +34,7 @@ export default function RecommendationScreen({
   React.useEffect(() => {
     // make post request to backend server
     var promise = axios.post(
-      "http://localhost:8080/products",
+      "https://gift-recommender-api.herokuapp.com/products",
       {
         categories: Array.from(chosenCategories),
         price: route.params?.price,
