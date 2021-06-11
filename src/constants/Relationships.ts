6de93648dc08beb1c7333c7d4d998245c88ccc42
Raul <rp519@ic.ac.uk>
2021-06-11 01:27:24 +0100

    Added new relationship tag

--- src/constants/Relationships.ts
+++ src/constants/Relationships.ts
@@ -1,13 +1,15 @@
 import TagData from "../interfaces/TagData";
 
 const relationships: TagData[] = [
+  {title:"Spouse"},
+  {title:"Significant Other"},
   {title:"Parent"},
   {title:"Child"},
   {title:"Sibling"},
   {title:"Work Colleague"},
   {title:"Friend"},
-  {title:"Significant Other"},
-  { title: "Prefer not to say" },
+  {title:"Other"},
+  {title: "Prefer not to say" },
 ];
 
 export default relationships;
