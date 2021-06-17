f3f9d2e9063de22cff1a55d5034779a457ff6c57
Raul <rp519@ic.ac.uk>
2021-06-17 22:05:07 +0100

    Budget allocator works

--- src/screens/quiz/sender/BudgetScreen.tsx
+++ src/screens/quiz/sender/BudgetScreen.tsx
@@ -1,13 +1,9 @@
-import React, { useCallback, useState } from "react";
-import { View, Text, Pressable, TextInput, TouchableWithoutFeedback, Keyboard} from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, TouchableWithoutFeedback } from "react-native";
 import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import { styles } from "../../../styles/quiz";
-import { buttonStyles } from "../../../styles/buttons";
-import { white } from "../../../styles/Colors";
 import SliderMarker from "../../../components/Quiz/SliderMarker";
 import Question from "../../../components/Question";
-import axios from "axios";
-import LoadingData from "../../../components/LoadingData";
 import QuizNavigator from "../../../components/Quiz/QuizNavigator";
 
 export default function BudgetScreen({
@@ -17,44 +13,27 @@ export default function BudgetScreen({
   route: any;
   navigation: any;
 }) {
-  const [min, onChangeMin] = React.useState(0);
-  const [max, onChangeMax] = React.useState(1000);
+  const [min, setMin] = React.useState(0);
+  const [max, setMax] = React.useState(200);
   var [priceRange, setPriceRange] = useState([0, 200]);
-  var nextMin = 0
-  var nextMax = 1000
-
-  const HideKeyboard = ({ children }) => (
-    <TouchableWithoutFeedback onPress={() => {
-        // Keyboard.dismiss()
-        if (validMin(nextMin) && validMax(nextMax)) {
-          onChangeMin(nextMin)
-          onChangeMax(nextMax)
-        }
-        console.log("clicked out")
-    }}>
-      {children}
-    </TouchableWithoutFeedback>
-  );
-
-  // const DismissKeyboard = () => (
-  //   <TouchableWithoutFeedback 
-  //     onPress={() => 
-  //       Keyboard.dismiss()
-        
-  //     }>
-  //   </TouchableWithoutFeedback>
-  //   );
+  const [tempMin, setTempMin] = React.useState(0);
+  const [tempMax, setTempMax] = React.useState(200);
+  const [minError, setMinError] = React.useState(false);
+  const [maxError, setMaxError] = React.useState(false);
 
   const validMin = (min: Number) => {
-    return min < max
+    var res = min < tempMax;
+    setMinError(!res);
+    return res;
   };
 
   const validMax = (max: Number) => {
-    return min < max
+    var res = tempMin < max;
+    setMaxError(!res);
+    return res;
   };
 
   return (
-
     <View style={styles.viewCentered}>
       <Question questionText={"What price range should the gift be?"} />
       <View style={styles.space} />
@@ -74,8 +53,10 @@ export default function BudgetScreen({
         onValuesChangeFinish={(values) => {
           priceRange[0] = values[0];
           priceRange[1] = values[1];
-          onChangeMin(values[0])
-          onChangeMax(values[1])
+          setMin(values[0]);
+          setTempMin(values[0]);
+          setMax(values[1]);
+          setTempMax(values[1]);
         }}
         values={[Number(min), Number(max)]}
         trackStyle={{
@@ -93,56 +74,87 @@ export default function BudgetScreen({
         allowOverlap={true}
       />
 
-    <HideKeyboard>
-
-    <View style={styles.row}>  
-
-
-      <TextInput
-              placeholder="Min"
-              style={styles.numericInput}  
-              keyboardType={'numeric'}
-              textAlign={'center'}
-              maxLength={3}
-              value = {min.toString()}
-              onChangeText={e => {
-                nextMin = Number(e)
-              }} 
+      <View style={styles.row}>
+        <TextInput
+          placeholder="Min"
+          style={[
+            styles.numericInput,
+            { borderColor: minError ? "red" : "black" },
+          ]}
+          keyboardType={"numeric"}
+          textAlign={"center"}
+          maxLength={3}
+          value={tempMin.toString()}
+          onChangeText={(e) => {
+            if (!isNaN(Number(e))) {
+              setTempMin(Number(e));
+            }
+          }}
+          onBlur={() => {
+            if (validMin(tempMin) && validMax(tempMax)) {
+              setMin(tempMin);
+              setMax(tempMax);
+            }
+          }}
         />
 
         <TextInput
-              placeholder="Max"
-              style={styles.numericInput}  
-              keyboardType={'numeric'}
-              textAlign={'center'}
-              maxLength={3}
-              value = {max.toString()}
-              onChangeText={e => {
-                nextMax = Number(e)
-              }}
-        />  
-     
-    </View>  
-    </HideKeyboard>
+          placeholder="Max"
+          style={[
+            styles.numericInput,
+            { borderColor: minError ? "red" : "black" },
+          ]}
+          keyboardType={"numeric"}
+          textAlign={"center"}
+          maxLength={3}
+          value={tempMax.toString()}
+          onChangeText={(e) => {
+            if (!isNaN(Number(e))) {
+              setTempMax(Number(e));
+            }
+          }}
+          onBlur={() => {
+            if (validMin(tempMin) && validMax(tempMax)) {
+              setMin(tempMin);
+              setMax(tempMax);
+            }
+          }}
+        />
+      </View>
 
-    <View style={styles.space} />
-      <QuizNavigator
-        currentpage={{
-          pagename: "Budget",
-          params: { ...route.params },
-        }}
-        navigation={navigation}
-        prev={{
-          pagename: "SenderCategories",
-        }}
-        next={{
-          pagename: "SenderRecommendations",
-          params: { price: priceRange },
-        }}
-        pagenum={route.params.pagenum}
-        totalpages={route.params.totalpages}
-      />
+      <View style={styles.space} />
+      {minError || maxError ? (
+        <QuizNavigator
+          currentpage={{
+            pagename: "Budget",
+            params: { ...route.params },
+          }}
+          navigation={navigation}
+          prev={{
+            pagename: "SenderCategories",
+          }}
+          next={{ pagename: "" }}
+          pagenum={route.params.pagenum}
+          totalpages={route.params.totalpages}
+        />
+      ) : (
+        <QuizNavigator
+          currentpage={{
+            pagename: "Budget",
+            params: { ...route.params },
+          }}
+          navigation={navigation}
+          prev={{
+            pagename: "SenderCategories",
+          }}
+          next={{
+            pagename: "SenderRecommendations",
+            params: { price: priceRange },
+          }}
+          pagenum={route.params.pagenum}
+          totalpages={route.params.totalpages}
+        />
+      )}
     </View>
-
   );
 }
